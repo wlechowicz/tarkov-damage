a0d81c24a7af1ee7f2bec1deec809fae5f4ca220
Wojciech Lechowicz <wojciech.lechowicz@intive.com>
2021-09-16 20:27:12 +0200

    chore: add some comments

--- src/components/Body.js
+++ src/components/Body.js
@@ -29,41 +29,66 @@ export function Body({ damage }) {
     const [limbsHP, setLimbsHP] = useState(calcState());
 
     const doDamage = (limbID, amount) => {
+        // apply dmg to the limb that took it
         const overflow = limbs.find(limb => limb.id === limbID).applyDamage(amount);
+
         console.log('applied', amount, 'dmg to', limbID, 'with overflow', overflow);
+        
+        // if that limb overflowed (was zeroed already or just got zerod by this damage), distribute the dmg
         if (overflow > 0 && isAlive()) {
             distributeDamage(overflow, 1);
         }
+
+        // update UI
         setLimbsHP(calcState());
     }
 
     const distributeDamage = (amount, steps) => {
         console.log('loop', steps, 'distributing', amount, 'remaining damage that overflowed');
+        
+        // killswitch if something went wrong and dmg left didn't attenuate properly
         if (steps > 10) {
             console.log('\taborting, too many steps');
             return;
         }
+
+        // dmg is distributed to all non-black limbs, proportionally to limb's max HP to remaining body ratio
         const nonBlackLimbs = limbs.filter(limb => !limb.isBlacked());
         const maxHPofNonBlackLimbs = getMaxHPNotBlacked();
+
+        // *** logging only
         let sumOfProportionalDamages = 0;
         let sumOfLimbOverflows = 0;
+        // ***
+
+        // apply dmg limb by limb and accumulate total overflow dmg from all blacked limbs
         const overflow = nonBlackLimbs.reduce((overflow, limb) => {
+
             const proportionalLimbDamage = Math.round(amount * limb.maxHP / maxHPofNonBlackLimbs);
+            const limbOverflow = limb.applyDamage(proportionalLimbDamage);
+
+            // *** logging only
             console.log('\tadding', proportionalLimbDamage, 'damage to', limb.id, 'that has', limb.HP, 'hp');
             sumOfProportionalDamages += proportionalLimbDamage;
-            const limbOverflow = limb.applyDamage(proportionalLimbDamage);
             sumOfLimbOverflows += limbOverflow;
             if (limbOverflow > 0) {
                 console.log('\t', limb.id, 'overflowed', limbOverflow, 'damage');
             }
+            // ***
+
             return overflow + limbOverflow;
         }, 0);
+
+        // *** logging only
         if (sumOfProportionalDamages !== amount) {
             console.log('\t(error of', sumOfProportionalDamages - amount, 'damage due to rounding)');
         }
         if (sumOfLimbOverflows > 0) {
             console.log('\tnewly blacked limbs overflowed a total of', sumOfLimbOverflows, 'damage');
         }
+        // ***
+
+        // recursion - still some dmg to spread and still alive, keep going
         if (overflow > 0 && isAlive()) {
             distributeDamage(overflow, steps + 1);
         }
